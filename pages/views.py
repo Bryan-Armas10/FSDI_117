eefa53983bb21a3dee667895935366261fc2db1d
Bryan-Armas10 <bryan.armas@sdgku.edu>
2025-02-22 12:02:41 -0800

    Assigment 3

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 # Create your views here.
 def my_first_view(request):
@@ -7,3 +10,33 @@ def my_first_view(request):
 
 def about_view(request):
     return render(request, "pages/about_me.html")
+
+
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print("Valid form")
+
+            #name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            message_body = render_to_string("content/email.html", request.POST)
+
+            # send the email
+            send_mail(
+                "Portfolio email",
+                message,
+                email,
+                ['bryan.armas@sdgku.edu'],
+                html_message=message_body
+            )
+
+        else:
+            print("Invalid form")
+        
+    else:
+        form = ContactForm()
+
+    return render(request, 'pages/contact.html', {'form': form})
