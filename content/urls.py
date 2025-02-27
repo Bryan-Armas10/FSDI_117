41af745534a535016c77718ddfdb0af63eef2417
Bryan-Armas10 <bryan.armas@sdgku.edu>
2025-02-27 15:36:16 -0800

    Class 5

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
-    path("", views.project_list_view, name="project_list")
+    path("", views.project_list_view, name="project_list"),
+    path("new_project/", views.project_new_view, name="project_new")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- content/views.py
+++ content/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+#from django.views.generic import ListView
 from .models import Project
+from .forms import ProjectForm
 
 
 # Create your views here.
@@ -7,3 +9,12 @@ def project_list_view(request):
     my_projects = Project.objects.all()
 
     return render(request, 'content/project_list.html', {"projects": my_projects})
+
+def project_new_view(request):
+    form = ProjectForm()
+    return render(request, 'content/project_new.html', {'form': form})
+
+# class ProjectListView(ListView):
+#     model = Project
+#     template_name = "content/project_list.html"
+#     context_object_name = "projects"
